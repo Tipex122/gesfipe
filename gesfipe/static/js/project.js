36577d9d56dfad6e388cae91ecdcecd463c917a1
Tipex122 <xavierxlh@yahoo.fr>
2018-04-15 21:43:50 +0200

    Adjustment of files from GesFi

--- gesfipe/static/js/project.js
+++ gesfipe/static/js/project.js
@@ -19,3 +19,9 @@ Issues with the above approach:
 4. Undocumented: No mention in the documentation, or it's too hard for me to find
 */
 $('.form-group').removeClass('row');
+
+
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+
