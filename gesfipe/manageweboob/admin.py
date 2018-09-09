37fd5f4b9266e4cb07b5b1f64f399b670d7417a0
Tipex122 <xavierxlh@yahoo.fr>
2018-09-09 22:24:35 +0200

    Added WeboobModules Admin info

--- gesfipe/manageweboob/admin.py
+++ gesfipe/manageweboob/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import WeboobModules
+
+class WeboobModulesAdmin(admin.ModelAdmin):
+    list_display = ('name_of_module', 'description_of_module')
+
+
+admin.site.register(WeboobModules,)
