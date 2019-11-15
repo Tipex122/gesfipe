b5b83afbc306e6cddc1b4964e1d397210818bbc2
Xavier <xavier@phoenix>
2019-11-15 17:59:28 +0100

    init celery with redis

--- config/settings/base.py
+++ config/settings/base.py
@@ -302,3 +302,10 @@ LOGGING = {
 
 logging.config.dictConfig(LOGGING)
 # logger = logging.getLogger(__name__)
+
+# celery
+CELERY_BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TASK_SERIALIZER = 'json'
